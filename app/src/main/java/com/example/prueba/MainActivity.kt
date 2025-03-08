c9f238ab1a45c32cf36b0df2fc22745ae7388e04
Paola <acsapaola@gmail.com>
2025-03-08 18:50:29 -0500

    se crea la base para validar el email sea ingresado correctamente

--- app/src/main/java/com/example/prueba/MainActivity.kt
+++ app/src/main/java/com/example/prueba/MainActivity.kt
@@ -1,15 +1,41 @@
 package com.example.prueba
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.example.prueba.databinding.ActivityMainBinding
+import java.util.regex.Pattern
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val buttonIniciar = findViewById<Button>(R.id.button_iniciar)
+
+        buttonIniciar.setOnClickListener {
+            validateEmail()
+        }
+    }
+
+    private fun validateEmail() {
+        val email_text = findViewById<EditText>(R.id.text_view_gmail)
+        val email = email_text.text.toString()
+        if (!validarEmail(email)) {
+            email_text.setError("Email no válido")
+            // Aquí agregas la lógica de validación
+        }
+    }
+
+    fun validarEmail(email: String): Boolean {
+        val pattern: Pattern = Patterns.EMAIL_ADDRESS
+        return pattern.matcher(email).matches()
     }
 }
